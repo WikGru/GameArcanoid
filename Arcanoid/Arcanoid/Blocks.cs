6627f581d7c55b738bd190eee75fed5bfec2b02c
Wiktor Grudzina <w.grudzina@gmail.com>
2018-10-25 09:50:53 +0200

    zaczynam robic bloczki

---
+++ Arcanoid/Arcanoid/Blocks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcanoid
+{
+    class Blocks
+    {
+        //0 - non existatnt
+        //1 - orange one hit to destroy
+        //2 - red two hits to destroy
+        //3 - gray indestructible
+        private int state; 
+    }
+}
